c7e7333e1550ae15f4d5da7e094cf4681215717d
punarvasu510 <alekhya@nudgespot.com>
2016-10-27 02:16:19 +0530

    Basic examples

--- Example5.py
+++ Example5.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from flask import Flask,render_template,request, 
+from flask import Flask, render_template ,request , redirect
 
 client = MongoClient("localhost",27017)
 db = client.plugin2710
@@ -22,7 +22,7 @@ def create():
 @app.route("/create_record",methods=['POST'])
 def create_record():
 	name = request.values.get("name")
-	age = request.values.get("sem")
+	sem = request.values.get("sem")
 	print "----------Record created!!!!----------"
 	student.insert({"name":name,"sem":sem})
 	return redirect("/")
@@ -38,23 +38,6 @@ def delete_record():
 	print "----------Record deleted!!!!----------"
 	student.remove({key:value})
 	return redirect("/")
-
-
-@app.route("/update")
-def update():
-	return render_template('Update_record.html')
-
-@app.route("/update_record")
-def update_record():
-	print ("------------------")
-	print request.values
-
-	ID = request.values.get("id")
-	key = request.values.get("key")
-	value = request.values.get("value")
-	student.update({"_id":ObjectId(ID)}, {'$set':{key:value}})
-	return 
-
 
 if __name__ == "__main__":
 	app.run(debug=True)
